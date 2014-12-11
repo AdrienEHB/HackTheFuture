1b1146182665734112ac7040ced63117da2daebf
AdrienEHB <Adrien.schautteet@student.ehb.be>
2014-12-11 11:08:27 +0100

    Restore commit
    Libraries were missing in previous versions, removed gitignore

--- HackTheFuture/app/src/main/java/be/ehb/adrienschautteet/hackthefuture/ActivityOne.java
+++ HackTheFuture/app/src/main/java/be/ehb/adrienschautteet/hackthefuture/ActivityOne.java
@@ -2,6 +2,7 @@ package be.ehb.adrienschautteet.hackthefuture;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import java.util.List;
@@ -19,7 +20,7 @@ public class ActivityOne extends Activity implements View.OnClickListener {
         setContentView(R.layout.activity_activity_one);
 
         RestAdapter restAdapter = new RestAdapter().Builder()
-                .setEndPoint("https://apis-explorer.appspot.com/apis-explorer/?base=https://hackaton-backend.appspot.com/_ah/api#p/cities/v1/")
+                .setEndPoint("https://hackaton-backend.appspot.com/_ah/api")
                 .build();
 
         GitHubService service = restAdapter.create(GitHubService.class);
@@ -28,6 +29,7 @@ public class ActivityOne extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        List<City> repos = service.listRepos("octocat");
+        List<City> cities = service.cities();
+        Log.v(getClass().getName(), cities.toString());
     }
 }
--- HackTheFuture/app/src/main/java/be/ehb/adrienschautteet/hackthefuture/GitHubService.java
+++ HackTheFuture/app/src/main/java/be/ehb/adrienschautteet/hackthefuture/GitHubService.java
@@ -3,13 +3,12 @@ package be.ehb.adrienschautteet.hackthefuture;
 import java.util.List;
 
 import retrofit.http.GET;
-import retrofit.http.Path;
 
 /**
  * Created by adrienschautteet on 11/12/14.
  */
 public interface GitHubService {
 
-    @GET("/cities/{city}")
-    List<City> cities(@Path("name") String name);
+    @GET("cities/v1/citycollection")
+    List<City> cities();
 }
